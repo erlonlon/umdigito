873aae8314dd66f1cbc7b491ca6d172d3e0a4a5d
erlon <erlonlon@gmail.com>
2013-10-06 21:39:31 -0300

    primeiro commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Umdigito::Application.config.secret_token = '7fdf21c2adc86fa85e3f374cbe0295c5fb8a837748ae2581ba84b7c28078c2fe309d1995a52d94a553c09c1c0b409d88ff449ea9b79fc47e5896ab952763ae1a'
